21dc6c8718a2c7ab7c30d49dfe6bdd11d20e8044
jonathanharijanto <harijanj@oregonstate.edu>
2016-03-05 10:51:40 -0800

    file structure setup

---
+++ src/main/java/models/Money.java
@@ -0,0 +1,8 @@
+package models;
+
+/**
+ * Created by jonathan on 3/5/2016.
+ */
+public class Money {
+
+}
---
+++ src/main/java/models/Person.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by jonathan on 3/5/2016.
+ */
+public class Person {
+}

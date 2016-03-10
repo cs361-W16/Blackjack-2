d768a857dd9ed24d230e45ec45f1e90de5997d8a
Brandon To <tobrandon17@gmail.com>
2016-03-10 03:31:47 -0800

    commented some test code

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -20,8 +20,7 @@ public class Game {
     public java.util.List<Card> deck = new ArrayList<>();
 
 
-
-    public Game(){
+    public Game() {
     }
 
 
@@ -30,7 +29,7 @@ public class Game {
 
         // For normal version, we have: 2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A
         // Total: 52
-            // i < 15 means until 14 (each suit 14 cards)
+        // i < 15 means until 14 (each suit 14 cards)
         for (int i = 2; i < 15; i++) {
             deck.add(new Card(i, Suit.Clubs));
             deck.add(new Card(i, Suit.Hearts));
@@ -39,7 +38,7 @@ public class Game {
         }
     }
 
-    public void clearDeck(){
+    public void clearDeck() {
         deck.clear();
 
     }
@@ -54,7 +53,7 @@ public class Game {
     //Player Hand Functions
     // Remove the top card from cardList
     public Card pop(java.util.List<Card> cardList) {
-        return cardList.remove(cardList.size()-1);
+        return cardList.remove(cardList.size() - 1);
     }
 
     // Put one card into hand
@@ -62,27 +61,45 @@ public class Game {
         person1.addHand(pop(deck));
     }
 
-    public void Deal(){
+    /*
+    public void hit(){
+        canSplit = false;
+        canDoubleDown = false;
+        Card currentCard = myDeck.Deal();
+        hands.get(1).add(currentCard);
+        player1.hit(currentCard);
+        roundCheck();
+    }
+    public void hitSplit(){
+        Card currentCard = myDeck.Deal();
+        hands.get(2).add(currentCard);
+        player1.hitSplit(currentCard);
+        roundCheck();
+    }
+     */
+
+    public void Deal() {
         hit(player);
         hit(player);
         hit(dealer);
         hit(dealer);
     }
 
-    public void clearHands(){
+    public void clearHands() {
         player.clearHand();
         dealer.clearHand();
     }
     //End of Player Hand Functions
 
     //Turns
-    public void Hold(){
-       dealerAction();
+    public void Hold() {
+        dealerAction();
 
         //SET ALL BUTTONS TO FALSE IF IMPLIMENTING HERE
     }
+
     //Sets player and dealer hands and creates a new deck
-    public void newRound(){
+    public void newRound() {
         clearHands();
         clearDeck();
         buildDeck();
@@ -90,26 +107,26 @@ public class Game {
         Deal();
     }
 
-    public int endRound(){
+    public int endRound() {
         if (player.countCards() > 21) {
             System.out.println("Player Busted");
             return 0;
-        }else if(player.countCards() > dealer.countCards()){
+        } else if (player.countCards() > dealer.countCards()) {
             System.out.println("Player Wins");
             return 1;
-        }else if (player.countCards() == dealer.countCards()){
+        } else if (player.countCards() == dealer.countCards()) {
             System.out.println("Tie Game");
             return 2;
-        }else{
+        } else {
             System.out.println("Dealer Wins");
             return 3;
         }
     }
 
-    public int dealerAction(){
-        while(dealer.countCards() < 17){
+    public int dealerAction() {
+        while (dealer.countCards() < 17) {
             hit(dealer);
-            if(dealer.countCards() > 21){
+            if (dealer.countCards() > 21) {
                 System.out.println("Dealer Busted");
                 return 1;
             }
@@ -120,27 +137,22 @@ public class Game {
     //End of Turns
 
     //Money things
-    public void beginingBalance(){
+    public void beginingBalance() {
         player.addMoney(100);
     }
 
-    public void Bet(){
+    public void Bet() {
         player.loseMoney(2);
-        pot +=2;
+        pot += 2;
     }
 
-    public void winBet(){
+    public void winBet() {
         player.addMoney(pot);
         pot = 0;
     }
 
-    public void doubleDown(){
+    public void doubleDown() {
 
     }
-
-<<<<<<< HEAD
-    
-=======
-    //End of Money things
->>>>>>> b1f3b6007177924fd9eaf5fb33abbc959283be86
 }
+    //End of Money things
